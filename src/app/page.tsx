7e7ac956de97d854f72081df4df4c9c021120c6f
Rowan Murray <rowanomurray@gmail.com>
2025-04-26 23:18:42 +0100

    style: responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
     return (
         <>
             <nav className="bg-navy px-4">
-                <div className="max-w-7xl mx-auto flex justify-between py-2">
+                <div className="max-w-7xl px-8 mx-auto flex justify-between py-2">
                     <Image
                         src="/logo.png"
                         alt="Warboys Table Tennis Club Logo"
@@ -44,36 +44,44 @@ export default function Home() {
                     <div className="text-light flex items-center gap-4">
                         <a href={`mailto:${clubEmail}`}>
                             <div className="flex gap-2">
-                                <Mail />
-                                <div>
-                                    <p className="font-semibold">Send Mail</p>
-                                    <p>{clubEmail}</p>
+                                <div className="hidden md:block">
+                                    <Mail />
+                                </div>
+                                <div className="flex flex-col justify-center">
+                                    <p className="font-semibold">Send Email</p>
+                                    <p className="hidden md:block">
+                                        {clubEmail}
+                                    </p>
                                 </div>
                             </div>
                         </a>
                     </div>
                 </div>
             </nav>
-            <section className="flex items-center max-w-7xl mx-auto py-16">
+            <section className="flex flex-col md:flex-row items-center max-w-7xl px-8 mx-auto py-16">
                 <div className="grow basis-1/2 flex flex-col gap-4">
-                    <h1 className="text-6xl font-black">
+                    <h1 className="text-3xl md:text-6xl font-black">
                         Warboys Table Tennis Club
                     </h1>
-                    <p className="flex font-semibold text-lg items-center gap-4">
-                        <span className="flex items-center">
+                    <p className="flex flex-col font-semibold text-lg gap-4">
+                        <span className="flex gap-2 items-center">
                             <Pin />
-                            <span>{clubAddressShort}</span>
+                            <span className="text-nowrap">
+                                {clubAddressShort}
+                            </span>
                         </span>
-                        <span className="flex items-center">
+                        <span className="flex gap-2 items-center">
                             <Event />
-                            <span>Thursdays, 7:30pm onwards</span>
+                            <span className="text-nowrap">
+                                Thursdays, 7:30pm onwards
+                            </span>
                         </span>
                     </p>
                 </div>
                 <PhotoCollage />
             </section>
             <section className="bg-blue">
-                <div className="max-w-7xl mx-auto py-16 flex justify-between">
+                <div className="max-w-7xl px-8 mx-auto py-16 flex flex-col md:flex-row justify-between gap-16">
                     <div>
                         <h2 className="text-4xl font-bold">
                             {PRACTICE_NIGHTS_TITLE}
@@ -90,16 +98,15 @@ export default function Home() {
                     </div>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18573.133972422787!2d-0.08253697270635704!3d52.39723498913108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4877e0df452b9b03%3A0xe9c2da897813fd03!2sWarboys%20Sports%20%26%20Social%20Club!5e1!3m2!1sen!2suk!4v1745703533290!5m2!1sen!2suk"
-                        width="600"
-                        height="450"
                         style={{ border: 0 }}
                         allowFullScreen={false}
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
+                        className="w-full h-96 md:w-1/2 md:h-auto mt-16 md:mt-0"
                     ></iframe>
                 </div>
             </section>
-            <section className="max-w-7xl mx-auto py-16">
+            <section className="max-w-7xl px-8 mx-auto py-16">
                 <h2 className="text-4xl font-bold">FAQs</h2>
                 {FAQs.map((faq) => (
                     <>
@@ -111,7 +118,7 @@ export default function Home() {
                 ))}
             </section>
             <footer className="bg-navy text-light py-4">
-                <div className="max-w-7xl mx-auto flex gap-8">
+                <div className="max-w-7xl px-8 mx-auto flex flex-col md:flex-row gap-8">
                     <div>
                         <Image
                             src="/logo.png"
@@ -122,15 +129,15 @@ export default function Home() {
                     </div>
                     <div className="max-w-80">
                         <p className="text-2xl mb-6 font-semibold">Contact</p>
-                        <div className="grid grid-cols-[auto_1fr] gap-x-2 gap-y-4 justify-center">
-                            <div className="mx-auto">
+                        <div className="grid grid-cols-1 md:grid-cols-[auto_1fr] gap-x-2 gap-y-4 justify-center">
+                            <div className="mx-auto hidden md:block">
                                 <Mail />
                             </div>
                             <div>
                                 <p className="font-semibold">Send Mail</p>
                                 <p>{clubEmail}</p>
                             </div>
-                            <div className="mx-auto">
+                            <div className="mx-auto hidden md:block">
                                 <Pin />
                             </div>
                             <div>
